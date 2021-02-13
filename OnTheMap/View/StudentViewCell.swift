8fdfd8477222c0876fe5c3305aa749546521c657
Jeremy MacLeod <jeremycmacleod@icloud.com>
2021-02-13 21:51:07 +0000

    Implement code for StudentViewCell

--- OnTheMap/View/StudentViewCell.swift
+++ OnTheMap/View/StudentViewCell.swift
@@ -7,4 +7,29 @@
 
 import UIKit
 
-
+class StudentViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var urlLabel: UILabel!
+    @IBOutlet weak var pinImage: UIImageView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        configure(with: .none)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configure(with student: StudentLocation?) {
+        if let student = student {
+            nameLabel?.text = "\(student.firstName)" + " \(student.lastName)"
+            urlLabel?.text = student.mediaURL
+            pinImage.image = UIImage(named: "icon_pin")
+        } else {
+            nameLabel?.text = "Problem with the data"
+        }
+    }
+}
